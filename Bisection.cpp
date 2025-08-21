ac7501c2f85952186cfdf9d4a53f97dd662e2ce9
Muaj <m.codevibe@gmail.com>
2025-08-21 17:21:00 +0600

    Add files via upload

---
+++ Bisection.cpp
@@ -0,0 +1,56 @@
+// C++ program for implementation of Bisection Method for
+// solving equations
+#include<bits/stdc++.h>
+using namespace std;
+#define EPSILON 0.001
+
+// An example function whose solution is determined using
+// Bisection Method. The function is x^3 - x^2 + 2
+double func(double x)
+{
+	return x*x*x - x - 11;
+}
+
+// Prints root of func(x) with error of EPSILON
+void bisection(double a, double b)
+{
+	if (func(a) * func(b) >= 0)
+	{
+		cout << "You have not assumed right a and b\n";
+		return;
+	}
+
+	double c = a;
+	while ((b-a) >= EPSILON)
+	{
+		// Find middle point
+		c = (a+b)/2;
+
+		// Check if middle point is root
+		if (func(c) == 0.0)
+			break;
+
+		// Decide the side to repeat the steps
+		else if (func(c)*func(a) < 0)
+			b = c;
+		else
+			a = c;
+	}
+	cout << "The value of root (Bisection method) is : " << c;
+}
+
+// Driver program to test above function
+int main()
+{
+	 cout << "Name: Muaj" << endl;
+    cout << "ID No: 1449";
+	// Initial values assumed
+	   double a, b;
+    cout << "\nEnter the first guess (a): ";
+    cin >> a;
+    cout << "Enter the second guess (b): ";
+    cin >> b;
+	bisection(a, b);
+	return 0;
+}
+
